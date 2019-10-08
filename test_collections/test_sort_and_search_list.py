b99d03fdb7f53930a58eaa88c54828869746f893
Omar <gomar@dmacc.edu>
2019-10-08 00:26:25 -0500

    Test passed

--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -1,12 +1,11 @@
 import unittest
-# from unittest.mock import patch
 import fun_with_collections.sort_and_search_list as basic_list_exception
 
 
 class MyTestCase(unittest.TestCase):
     def test_make_list(self):
-        self.assertTrue(basic_list_exception.sort_list([6, 3, 9, 7]))
-        basic_list_exception.search_list([3, 5, 7, 9])
+        self.assertTrue(basic_list_exception.sort_list([5, 3, 6, 8,9,10, 4]))
+        basic_list_exception.search_list([3])
 
 
 if __name__ == '__main__':
